c6336d6fc48c50e11f1b7efecb88acc5985351bb
JuanSMarin <juansmarin269@gmail.com>
2024-04-14 15:08:23 -0500

    Actualizacion mesa

--- Factura.cs
+++ Factura.cs
@@ -22,7 +22,8 @@ namespace Taller_2
                 Console.WriteLine($"No hay productos consumidos en la mesa {numeroMesa}.");
                 return;
             }
-             
+                
+
             List<Producto> productosConsumidos = _productosConsumidosPorMesa[numeroMesa];
              
             //ASCII 
